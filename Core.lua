78782736b274d4a100141317fe011baf7b0e9070
mosesap <mosesap20@gmail.com>
2019-09-30 19:42:16 -0400

    realized that I should be using a status bar w/ an OnUpdate

--- Core.lua
+++ Core.lua
@@ -21,34 +21,48 @@ end
 local prev_energy = 0
 local unit_energy = 0
 local prev_energy_tick = 0
-local tick_increment = 1.9
+
+local prev_time_tick = 0
+local tick_increment = 0.5
 -------------------------------------------------------------
 --REGION CREATE AUBAR FRAME
 -------------------------------------------------------------
-local AuBar = CreateFrame('StatusBar', 'AuBar', UIParent)
-AuBar:SetSize(300,360)
-AuBar:SetPoint('Center', UIParent, 'Center')
+local BaseFrame = CreateFrame('Frame', 'BaseFrame', UIParent)
+BaseFrame:SetSize(300,360)
+BaseFrame:SetPoint('Center', UIParent, 'Center')
 --generic scipt caller, calls appropiate function on event
-AuBar:SetScript("OnEvent", function(self, event, ...)
+BaseFrame:SetScript("OnEvent", function(self, event, ...)
     --print(event, unpack{...})
 	return self[event](self, event, ...)
 end)
-AuBar:RegisterEvent("PLAYER_LOGIN")
-AuBar:RegisterEvent("PLAYER_LOGOUT")
+--BaseFrame:SetScript("OnUpdate", TickerOnUpdate)
+BaseFrame:RegisterEvent("PLAYER_LOGIN")
+BaseFrame:RegisterEvent("PLAYER_LOGOUT")
 
 -------------------------------------------------------------
---REGION AUBAR EVENT HANDLERS
+--REGION BASE FRAME EVENT HANDLERS
 -------------------------------------------------------------
-function AuBar.PLAYER_LOGIN(self, event)
+function BaseFrame.PLAYER_LOGIN(self, event)
         print("welcome ", UnitName("player").."!")
         self:RegisterEvent("UNIT_POWER_UPDATE")
         self:UNIT_POWER_UPDATE(nil, "player", "ENERGY")
         
-        local TickerFrame = CreateFrame("Frame")
-        TickerFrame:SetScript("OnUpdate", TickerOnUpdate)
+        local red = .5
+        local green = .5
+        local blue = .5
+        local opacity = 1
+        local tick = 0
+        local AuBar = CreateFrame("StatusBar", "")
+        AuBar:SetScript("OnUpdate", TickerOnUpdate)
+        AuBar:SetStatusBarColor(red, green, blue, opacity)
+        --may need to set a texture here
+        AuBar:Show()
+        local minValue
+        local maxValue
+        minValue, maxValue = StatusBar:GetMinMaxValues()
 end
 
-function AuBar.UNIT_POWER_UPDATE(self, event, unit, powertype)
+function BaseFrame.UNIT_POWER_UPDATE(self, event, unit, powertype)
     unit_energy = UnitPower("player")
     if unit_energy ~= prev_energy then
         --do something
@@ -56,13 +70,21 @@ function AuBar.UNIT_POWER_UPDATE(self, event, unit, powertype)
     prev_energy = unit_energy
 end
 
+-------------------------------------------------------------
+--REGION AUBAR UPDATE HANDLER
+-------------------------------------------------------------
 function TickerOnUpdate(self)
-     unit_energy = UnitPower("player", PowerTypeIndex)
     local now = GetTime()
-    if unit_energy > prev_energy or now >= prev_energy_tick + 2 then
+    if now >= prev_energy_tick + 2 then
         prev_energy_tick = now
+        prev_time_tick = now
         print("now")
-    end
-    --% game time and only increment the bar if the time has moved by > the increment
-    prev_energy = unit_energy
+        tick = 0
+        AuBar:SetValue(tick)
+    elseif now >= prev_time_tick + tick_increment then
+        print("tick")
+        tick += tick_increment
+        AuBar:SetValue(tick)
+        prev_time_tick = now
+    end     
 end
