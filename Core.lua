b69f8e3845a139c278f3ecee70424ce0f960f3ee
mosesap <mosesap20@gmail.com>
2020-10-11 23:02:09 -0400

    TODO Druid support

--- Core.lua
+++ Core.lua
@@ -1,78 +1,55 @@
-SLASH_RELOADUI1 = '/rl' -- for quicker reloading
-SlashCmdList.RELOADUI = ReloadUI
-
-SLASH_FRAMESTK1 = '/fs' -- for quicker access to frame stack
-SlashCmdList.FRAMESTK = function()
-    LoadAddOn('Blizzard_DebugTools')
-    FrameStackTooltip_Toggle()
-end
--- to be able to use the left and right arrows in the edit box
--- with out rotating your character!
-for i = 1, NUM_CHAT_WINDOWS do
-    _G['ChatFrame'..i..'EditBox']:SetAltArrowKeyMode(false)
-end
-
 local function CreateBar(name, previous)
-	local f = CreateFrame("StatusBar", "Fizzle"..name, UIParent)
-	f:SetSize(200, 30)
+	local frame = CreateFrame("StatusBar", "Fizzle"..name, UIParent)
+	frame:SetSize(200, 30)
 	if not previous then
-		f:SetPoint("LEfT", 10, 0)
+		frame:SetPoint("LEFT", 10, 0)
 	else
-		f:SetPoint("TOP", previous, "BOTTOM")
+		frame:SetPoint("TOP", previous, "BOTTOM")
 	end
-	f:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-	f.Text = f:CreateFontString()
-	f.Text:SetFontObject(GameFontNormal)
-	f.Text:SetPoint("CENTER")
-	f.Text:SetJustifyH("CENTER")
-	f.Text:SetJustifyV("CENTER")
-	return f
+	frame:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+	frame.Text = frame:CreateFontString()
+	frame.Text:SetFontObject(GameFontNormal)
+	frame.Text:SetPoint("CENTER")
+	frame.Text:SetJustifyH("CENTER")
+	frame.Text:SetJustifyV("CENTER")
+	return frame
 end
 
-local function UpdateTicker(self) 
-    local curr_nrg = UnitPower("player")
-    local now = GetTime()
-    local bar_tick = math.floor((now - self.prev_tick)  * 100)
-    if bar_tick > 190 or bar_tick < 10 then 
-        self:SetStatusBarColor(1, .1, .9) 
-    else 
-        self:SetStatusBarColor(255, 215, 0)
+local function ConfigureBar(bar)
+    local function UpdateBar(self)
+        self.curr_nrg = UnitPower("player")
+        self.now = GetTime()
+        self.bar_tick = math.floor((self.now - self.prev_tick)  * 100)
+        if self.bar_tick > 185 then 
+            self:SetStatusBarColor(242/255, 75/255, 75/255) 
+        else 
+            self:SetStatusBarColor(255, 215, 0)
+        end
+        if self.now >= self.prev_tick + 2 or self.curr_nrg > self.prev_nrg then
+            self.prev_tick = self.now
+            self.bar_tick = 0 
+        end
+        self.prev_nrg = self.curr_nrg
+        self.Text:SetText(self.curr_nrg)  
+        self:SetValue(self.bar_tick)
     end
-    if now >= self.prev_tick + 2 or curr_nrg > self.prev_nrg then
-        self.prev_tick = now
-        bar_tick = 0 
-    end  
-    if curr_nrg + 20 < UnitPowerMax("player") then
-        self.Text:SetText(curr_nrg + 20)
-    else
-        self.Text:SetText(UnitPowerMax("player"))
-    end
-    self.prev_nrg = curr_nrg
-    self:SetValue(bar_tick)
-end
-
-if "Rogue" == UnitClass("player") or "Druid" == UnitClass("player") then
-    local Au_Bar = CreateBar('GoldBar')
-    print("here")
-    Au_Bar:SetMinMaxValues(0, 200)
-    Au_Bar:SetMovable(true)
-    Au_Bar:EnableMouse(true)
-    Au_Bar:RegisterForDrag("LeftButton")
-    Au_Bar:SetScript("OnDragStart", Au_Bar.StartMoving)
-    Au_Bar:SetScript("OnDragStop", Au_Bar.StopMovingOrSizing)
-    Au_Bar.prev_nrg = 0
-    Au_Bar.prev_tick = 0
-    Au_Bar:SetScript("OnUpdate", function(self, event, ...)
-        UpdateTicker(self)
+    
+    bar:SetMinMaxValues(0, 200)
+    bar:SetMovable(true)
+    bar:EnableMouse(true)
+    bar:RegisterForDrag("LeftButton")
+    bar:SetScript("OnDragStart", bar.StartMoving)
+    bar:SetScript("OnDragStop", bar.StopMovingOrSizing)
+    bar.prev_nrg = 0
+    bar.prev_tick = 0
+    bar.curr_nrg = 0
+    bar.now = 0
+    bar.bar_tick = 0
+    bar:SetScript("OnUpdate", function(self, event, ...)
+        UpdateBar(self)
     end)
-    --if "Druid" == UnitClass("player") then
-    --    Au_Bar:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
-    --    Au_Bar:SetScript("OnEvent"), function(self, event, ...)
-    --        if event == "UPDATE_SHAPESHIFT_FORM" and 3 = GetShapeshiftForm() then
-    --           Au_Bar:Show()
-    --        elseif event == "UPDATE_SHAPESHIFT_FORM" and 3 ~= GetShapeshiftForm() then
-    --            Au_Bar:Hide()
-    --        end
-    --    end)
-    --end
+end
+
+if "Rogue" == UnitClass("player") then 
+    ConfigureBar(CreateBar('GoldBar'))
 end
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Gold-Bar
-Classic WoW Energy Ticker
+# Gold-Bar Classic WoW Energy Ticker For Rogues
 
--Currently works for rogues. Bar will change colors as energy is ticking.
--Bar can be dragged around the screen.
+Currently works for Rogues; Druid support TBD, need to level a Druid to 20 to test.  
+Changes colors right before energy is ticks.  
+Displays current energy. *I messed around with displaying predicted energy upon next tick, but it can be confusing since energy ticks are not always +20*
+Can be dragged around the screen.  
 
-Download files, move Core.lua and Gold-Bar.toc into your AddOns folder
+Download files, move the Gold-Bar folder into your AddOns folder.
