d77f91db176bdd57d2efc0e7f4ba0c49c5c110e8
mosesap <mosesap20@gmail.com>
2020-10-13 21:55:27 -0400

    added combo point tracker. wanted to add 5 individual circles, but the bar w/ changing colors will do. TODO: fade when out of combat, i think the opacity is a 4th parameter when changing colors of the bars

--- Core.lua
+++ Core.lua
@@ -1,12 +1,11 @@
-local function CreateBar(name, previous)
-	local frame = CreateFrame("StatusBar", "Fizzle"..name, UIParent)
-	frame:SetSize(200, 30)
-	if not previous then
+local function CreateBar(name, type, texture, anchor)
+	local frame = CreateFrame(type, name, UIParent)
+	if not anchor then
 		frame:SetPoint("LEFT", 10, 0)
 	else
-		frame:SetPoint("TOP", previous, "BOTTOM")
+		frame:SetPoint("TOPLEFT", anchor, "BOTTOMLEFT")
 	end
-	frame:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+	frame:SetStatusBarTexture(texture)
 	frame.Text = frame:CreateFontString()
 	frame.Text:SetFontObject(GameFontNormal)
 	frame.Text:SetPoint("CENTER")
@@ -15,7 +14,7 @@ local function CreateBar(name, previous)
 	return frame
 end
 
-local function ConfigureBar(bar)
+local function ConfigureGoldBar(bar)
     local function UpdateBar(self)
         self.curr_nrg = UnitPower("player")
         self.now = GetTime()
@@ -33,7 +32,7 @@ local function ConfigureBar(bar)
         self.Text:SetText(self.curr_nrg)  
         self:SetValue(self.bar_tick)
     end
-    
+    bar:SetSize(200, 30)
     bar:SetMinMaxValues(0, 200)
     bar:SetMovable(true)
     bar:EnableMouse(true)
@@ -50,6 +49,42 @@ local function ConfigureBar(bar)
     end)
 end
 
+local function ConfigureComboBar(bar, point)
+    local function UpdateBar(self)
+        self.curr_points = GetComboPoints("player", "target")
+        self:SetValue(self.curr_points)
+        self.Text:SetText(self.curr_points) 
+        if self.curr_points == 0 then
+            self:SetStatusBarColor(255, 0, 0) 
+        elseif self.curr_points == 1 then
+            self:SetStatusBarColor(246/255, 189/255, 192/255) 
+        elseif self.curr_points == 2 then
+            self:SetStatusBarColor(241/255, 149/255, 155/255) 
+        elseif self.curr_points == 3 then
+            self:SetStatusBarColor(240/255, 116/255, 112/255) 
+        elseif self.curr_points == 4 then
+            self:SetStatusBarColor(234/255, 76/255, 70/255) 
+        else
+            self:SetStatusBarColor(220/255, 28/255, 19/255)
+        end
+    end
+    bar.curr_points = 0
+    bar.Text:SetText(bar.curr_points)  
+    bar:SetSize(200, 30)
+    bar:SetMinMaxValues(0, 5)
+    bar:SetValue(0)
+    bar:SetStatusBarColor(242/255, 75/255, 75/255) 
+    bar:SetScript("OnEvent", function(self, event, ...)
+        if event == "UNIT_POWER_UPDATE" then
+            UpdateBar(self)
+        end
+    end)
+    bar:RegisterEvent("UNIT_POWER_UPDATE")
+end
+
 if "Rogue" == UnitClass("player") then 
-    ConfigureBar(CreateBar('GoldBar'))
+    local GoldBar = CreateBar("GoldBar", "StatusBar", "Interface\\TargetingFrame\\UI-StatusBar")
+    ConfigureGoldBar(GoldBar)
+    local ComboBar = CreateBar("ComboBar", "StatusBar", "Interface\\TargetingFrame\\UI-StatusBar", GoldBar)
+    ConfigureComboBar(ComboBar)
 end
